09e9c527c379713696435bc77d53937ac84967c0
Oghosa1 <guerrilladev@gmail.com>
2025-04-15 21:18:43 +0100

    first push

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -5,54 +5,57 @@ import 'package:portfolio/models/project_model.dart';
 Color kGradient1 = Colors.purple;
 Color kGradient2 = Colors.pinkAccent;
 
-String imagePath = "images/aditya.jpeg";
+String imagePath = "images/user.png";
 
 //String data to modify
-String name = "Your Name";
+String name = "Isaac Uzzi";
 String username = "flutterroles";
 
 //Link to resume on Google Drive
 String resumeLink =
-    "https://drive.google.com/file/d/1uZPqMWva6pZxzQxsR_ILLZ_FxJr24QeY/view?usp=sharing";
+    "https://drive.google.com/file/d/1q2HHOKnBn_TaIPgh8WoyTvzTqmrYGW4x/view?usp=drive_link";
 
 //Contact Email
-String contactEmail = "mail@adityathakur.in";
+String contactEmail = "jarviz101@gmail.com";
 
 String aboutWorkExperience = '''
-Hello I am a freelancer
+I am excited to apply for the Flutter Developer position your company. With a strong background in software development and a passion for building efficient, scalable solutions, I am eager to contribute my skills to your team.
 
-He has previously been a speaker at the Flutter Global Summit'21, Flutter Vikings and events across colleges, universities (like VIT Chennai, Vellore, MIT-ADT), and clubs (IEEE APSIT, SFIT, GirlScript, GeeksforGeeks Student Chapter, Google CrowdSource, and more) here in India. 
+I have been developing software since 2018, with expertise in Flutter, JavaScript, Firebase  Realtime Database, Cloud Firestore, Node.js, Express.js, MongoDB, Java, Kotlin, and Dart. My experience spans full-stack development, mobile applications, and backend systems. I am highly adaptable, self-driven, and always eager to learn new technologies to stay ahead in the industry.
+
+Beyond technical skills, I am an analytical thinker who thrives in problem-solving and collaboration. My ability to work independently and manage projects effectively makes me a valuable addition to any team. I am particularly drawn to this opportunity because it is the step to learn more about software development and expanding my knowledge.
+
+I would welcome the chance to discuss how my experience and skills can benefit your team. Thank you for your time and consideration—I look forward to your response.
+ 
 ''';
 
 String aboutMeSummary = '''
-Aditya is a content creator and has over 5K subscribers on YouTube and a 700+ member community on Discord. 
-📺 YouTube Channel: watch.adityathakur.in
-🚀 Discord Community: discord.adityathakur.in
+I am a passionate Flutter developer with a strong background in software development. I have been developing software since 2018, with expertise in Flutter, JavaScript, Firebase Realtime Database, Cloud Firestore, Node.js, Express.js, MongoDB, Java, Kotlin, and Dart. My experience spans full-stack development, mobile applications, and backend systems. I am highly adaptable, self-driven, and always eager to learn new technologies to stay ahead in the industry. 
 ''';
 
-String location = "Dharamshala, India";
-String website = "adityathakur.in";
-String portfolio = "adityathakurxd";
-String email = "mail@adityathakur.in";
+String location = "Lagos, Nigeria";
+String website = "guerrilladev.xyz";
+String portfolio = "Isaac Uzzi";
+String email = "jarviz101@gmail.com";
 
 List<Project> projectList = [
   Project(
-      name: "Shades",
+      name: "Amazon Clone",
       description:
-          "Winner IEEE Mandi 'Go Online' Challenge at Electrothon, NIT Hamirpur.",
-      link: "https://github.com/adityathakurxd/shades"),
+          "Amzon Clone is a full-stack e-commerce application that mimics the core features of Amazon. Built with Node.js for the backend and MongoDB for data storage, the app offers a seamless shopping experience including user authentication, product browsing, shopping cart functionality, and secure checkout.",
+      link: "https://github.com/Oghosa1/Amazon_Clone"),
   Project(
-      name: "Fiasco",
+      name: "Amazon Clone Backend",
       description:
-          "Fiasco is one stop solution to help you manage Family expenses in one place!",
+          "Amzon Clone is a full-stack e-commerce application that mimics the core features of Amazon. Built with Node.js for the backend and MongoDB for data storage, the app offers a seamless shopping experience including user authentication, product browsing, shopping cart functionality, and secure checkout.",
       link: "https://github.com/adityathakurxd/fiasco"),
   Project(
-      name: "Fiasco",
+      name: "MovieMadness",
       description:
-          "Fiasco is one stop solution to help you manage Family expenses in one place!",
-      link: "https://github.com/adityathakurxd/fiasco"),
+          "MovieMadness is a streamlined mobile app designed to keep you updated on the latest and most popular films. With an intuitive interface, it offers real-time movie trends, reviews, and ratings, making it easy for movie enthusiasts to discover what's hot in theaters and beyond.",
+      link: "https://play.google.com/store/apps/details?id=com.GuerrillaDev.TrendingMovies"),
   Project(
-      name: "Flutter Portfolio",
-      description: "A template for Portfolio",
-      link: "https://github.com/adityathakurxd/flutter-portfolio")
+      name: "Weather app",
+      description: "is a simple weather app that provides real-time weather updates and forecasts for any location. With an intuitive interface, users can easily search for cities and view current conditions, hourly forecasts, and extended weather predictions.",
+      link: "https://github.com/Oghosa1/Weather")
 ];
